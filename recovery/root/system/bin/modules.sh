8fc9fa627cae87ccf6b5dc5f319cdd448ba56aab
rote66 <sscarsb@126.com>
2021-10-07 01:12:45 +0800

    modules:use modprobe instead insmod to avoid built-in kernel crash modules:remove bu520x1nvx modules (not necessary in recovery)

--- recovery/root/system/bin/modules.sh
+++ recovery/root/system/bin/modules.sh
@@ -1,19 +1,12 @@
 #!/system/bin/sh
 
-#Synaptics touchscreen
-insmod /sbin/modules/synaptics_tcm_i2c.ko
-insmod /sbin/modules/synaptics_tcm_core.ko
-insmod /sbin/modules/synaptics_tcm_touch.ko
-insmod /sbin/modules/synaptics_tcm_device.ko
-insmod /sbin/modules/synaptics_tcm_testing.ko
-insmod /sbin/modules/synaptics_tcm_reflash.ko
-insmod /sbin/modules/synaptics_tcm_recovery.ko
-insmod /sbin/modules/synaptics_tcm_diagnostics.ko
-
-insmod /sbin/modules/synaptics_tcm_core.ko
-
-#vibrator
-insmod /sbin/modules/ldo_vibrator.ko
-
-#bu520
-insmod /sbin/modules/bu520x1nvx.ko
+exec /system/bin/modprobe -a -d /sbin/modules/ \
+    ldo_vibrator &
+exec /system/bin/modprobe -a -d /sbin/modules/ \
+    synaptics_tcm_i2c synaptics_tcm_core synaptics_tcm_touch \
+    synaptics_tcm_device synaptics_tcm_testing synaptics_tcm_reflash \
+    synaptics_tcm_recovery synaptics_tcm_diagnostics &
+sleep 2
+chown system system /sys/bus/platform/devices/synaptics_tcm.0/synaptics_tcm/reflash/config_id
+chmod 0600 /sys/bus/platform/devices/synaptics_tcm.0/synaptics_tcm/reflash/config_id
+restorecon /sys/bus/platform/devices/synaptics_tcm.0/synaptics_tcm/reflash/config_id
